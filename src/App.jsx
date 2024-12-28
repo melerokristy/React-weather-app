fd789a9ae5b47bd613e1f1c713006f244ed8dbc8
kristy melero <165970194+melerokristy@users.noreply.github.com>
2024-12-28 15:41:08 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Weather from "./Weather";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
