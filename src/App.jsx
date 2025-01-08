a63333949303527cb4461d7909e339b0d4ad63f7
kristy melero <165970194+melerokristy@users.noreply.github.com>
2025-01-08 14:36:38 +0000

    removed default city

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather />
 
         <footer>
           This project was coded by{" "}
